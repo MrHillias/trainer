676d6f4dddc3f923a57f5cff52af4d7e38d9d704
Ilya <geil1997@mail.ru>
2025-03-17 21:11:57 +0300

    Фикс настроек доступа к бд

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize("db_metavision", "root", "Fud5e@$$", {
+module.exports = new Sequelize("db_metavision", "user", "Fud5e@$$", {
   host: "79.174.88.83",
   port: "19684",
   dialect: "postgres",
