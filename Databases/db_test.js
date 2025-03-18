81c5738c296b32e863d30ac289832d70797d0537
Ilya <geil1997@mail.ru>
2025-03-18 21:07:43 +0300

    Возвращаем все к исходному состоянию

--- Databases/db_test.js
+++ Databases/db_test.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  "user",
+  process.env.DB_USER,
   String(process.env.DB_PASSWORD),
   {
     host: process.env.DB_HOST,
