1433897380f3a88e05c9c0062ffcb83a028fbf74
Ilya <geil1997@mail.ru>
2025-03-18 20:25:57 +0300

    Еще логи

--- server.js
+++ server.js
@@ -55,7 +55,9 @@ app.get("/", (req, res) => {
 
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_USER:", process.env.DB_USER);
-console.log("Type of DB_PASSWORD:", typeof process.env.DB_PASSWORD);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
 
 // Запуск сервера
 app.listen(PORT, () => {
