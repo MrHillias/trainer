6f3885a59f65219d12912ab6fcad8e27377136d3
Ilya <geil1997@mail.ru>
2025-03-18 19:56:10 +0300

    Вывод пароля для отладки

--- server.js
+++ server.js
@@ -55,6 +55,7 @@ app.get("/", (req, res) => {
 
 console.log("DB_NAME:", process.env.DB_NAME);
 console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
 // Запуск сервера
 app.listen(PORT, () => {
