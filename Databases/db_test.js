d09d3a8d198a500afe304c303d7d9102be387274
Ilya <geil1997@mail.ru>
2025-03-19 21:16:02 +0300

    encoding

--- Databases/db_test.js
+++ Databases/db_test.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  String(process.env.DB_PASSWORD),
+  encodeURIComponent(process.env.DB_PASSWORD),
   {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
