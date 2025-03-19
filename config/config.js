f6ebb9631d16cf7b4ad166c0d6346b473d7d7ff4
Ilya <geil1997@mail.ru>
2025-03-19 20:47:38 +0300

    config update

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config(); // Загружаем переменные окру�
 
 module.exports = {
   development: {
-    username: process.env.DB_NAME,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
